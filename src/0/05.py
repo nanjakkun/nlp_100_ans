dc77648cc9c957f33d2e791640668dfc79aa529d
KIMURA Takahiro <nanjakkun@gmail.com>
2024-03-17 07:28:45 +0900

    Solve ploblem 05

---
+++ src/0/05.py
@@ -0,0 +1,15 @@
+# 与えられたシーケンス（文字列やリストなど）からn-gramを作る関数を作成せよ．
+# この関数を用い，”I am an NLPer”という文から単語bi-gram，文字bi-gramを得よ．
+
+def word_n_gram(text, num):
+  words = text.split(" ")
+  return list(map(lambda i: words[i:i+num], range(len(words))))
+
+def char_n_gram(text, num):
+  words = text.replace(" ", "")
+  return list(map(lambda i: words[i:i+num], range(len(words))))
+
+text1 = "I am an NLPer"
+
+print(word_n_gram(text1, 2))
+print(char_n_gram(text1, 2))
