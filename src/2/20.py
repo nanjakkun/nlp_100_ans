dd4e1cbeb4ea2fe06388293aa17d46601af07508
KIMURA Takahiro <nanjakkun@gmail.com>
2024-03-21 09:57:06 +0900

    Solve Problem 21

--- src/2/20.py
+++ src/2/20.py
@@ -2,11 +2,18 @@
 # 問題21-29では，ここで抽出した記事本文に対して実行せよ．
 
 import json
+import os
 
-with open("jawiki-country.json", mode="r") as f:
-  for line in f:
-    obj = json.loads(line)
-    if obj['title'] == 'イギリス':
-      text = obj['text']
-      print(text)
-      break
+text = ""
+
+with open(os.path.dirname(__file__) + "/jawiki-country.json", mode="r") as f:
+    for line in f:
+        obj = json.loads(line)
+        if obj['title'] == 'イギリス':
+            text = obj['text']
+            break
+
+with open(os.path.dirname(__file__) + "/20answer.txt", "w") as f:
+    f.write(text)
+
+print(text)
